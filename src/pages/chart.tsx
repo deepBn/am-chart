9a7eddfc4aa2d33f19341d8df3fbeaa6169eb8cf
Rajdeep Barman <deepbn9@gmail.com>
2023-01-20 23:05:21 +0530

    Amer chatni

---
+++ src/pages/chart.tsx
@@ -0,0 +1,9 @@
+import dynamic from 'next/dynamic';
+
+const ChartComponent = dynamic(() => import('@/components/aamChat'), { ssr: false });
+
+export default function Chart() {
+  return (
+    <ChartComponent />
+  )
+}
